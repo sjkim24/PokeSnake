7983116dc3cabdcc330563775725771c0423280e
Sung Jin Kim <sjkim0421@gmail.com>
2015-12-22 15:14:45 -0500

    move check functions from view to game class

--- js/game.js
+++ js/game.js
@@ -12,4 +12,37 @@
     this.highScore = 0;
   };
 
+  Game.prototype.appleCheck = function (coord) {
+    if (coord.equals(this.board.apple.coord)) {
+      this.board.generateApple();
+      $(".apple").removeClass("apple");
+      this.score += 100;
+      if (this.score > this.highScore) {
+        this.highScore = this.score
+      }
+      return true;
+    }
+  };
+
+  Game.prototype.selfEatCheck = function (segments) {
+    if (segments.length > 1) {
+      var head = segments[0];
+      var rest = segments.slice(1, segments.length);
+      for (var i = 0; i <= rest.length - 1; i++) {
+        if (head.equals(rest[i])) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+  };
+
+  Game.prototype.outOfBoundCheck = function (segments) {
+    var head = segments[0];
+    if ((head.x < 0 || head.x > 19) || (head.y < 0 || head.y > 19)) {
+      return true;
+    }
+  };
+
 })();
--- js/view.js
+++ js/view.js
@@ -42,23 +42,22 @@
   };
 
   View.prototype.step = function() {
-    // var snake = this.game.board.snake;
     this.game.board.snake.turn();
     var oldsegments = _.clone(this.game.board.snake.segments);
     this.game.board.snake.move();
     var newsegments = _.clone(this.game.board.snake.segments);
-    if (this.checkApple(newsegments[0])) {
+    if (this.game.appleCheck(newsegments[0])) {
       this.game.board.snake.grow(oldsegments[oldsegments.length - 1]);
     }
     this.render(oldsegments, newsegments);
-    if (this.selfEatCheck(newsegments) || this.outOfBoundCheck(newsegments)) {
+    if (this.game.selfEatCheck(newsegments) || this.game.outOfBoundCheck(newsegments)) {
       this.gameOver();
     }
   };
 
   View.prototype.render = function (oldsegments, newsegments) {
     $("#score").html("Score: " + this.game.score);
-    $("#high-score").html("High Score: " + this.game.score);
+    $("#high-score").html("High Score: " + this.game.highScore);
     var snakeX = newsegments[0].x;
     var snakeY = newsegments[0].y;
     $("#" + snakeX).children("." + snakeY).addClass("snake");
@@ -68,44 +67,11 @@
     this.renderApple();
   };
 
-  View.prototype.checkApple = function (coord) {
-    if (coord.equals(this.game.board.apple.coord)) {
-      this.game.board.generateApple();
-      $(".apple").removeClass("apple");
-      this.game.score += 100;
-      if (this.game.score > this.game.highScore) {
-        this.game.highScore = this.game.score
-      }
-      return true;
-    }
-  };
-
   View.prototype.renderApple = function () {
     var apple = this.game.board.apple;
     var appleX = apple.coord.x;
     var appleY = apple.coord.y;
     $("#" + appleX).children("." + appleY).addClass("apple");
-  };
-
-  View.prototype.selfEatCheck = function (segments) {
-    if (segments.length > 1) {
-      var head = segments[0];
-      var rest = segments.slice(1, segments.length);
-      for (var i = 0; i <= rest.length - 1; i++) {
-        if (head.equals(rest[i])) {
-          return true;
-        }
-      }
-
-      return false;
-    }
-  };
-
-  View.prototype.outOfBoundCheck = function (segments) {
-    var head = segments[0];
-    if ((head.x < 0 || head.x > 19) || (head.y < 0 || head.y > 19)) {
-      return true;
-    }
   };
 
   View.prototype.gameOver = function () {
