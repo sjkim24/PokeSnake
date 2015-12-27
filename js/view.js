a033e8575d7f6c924306861b0e96f394bcf049bf
Sung Jin Kim <sjkim0421@gmail.com>
2015-12-27 16:21:56 -0500

    remove commented code

--- js/view.js
+++ js/view.js
@@ -81,10 +81,7 @@
       } else {
         if (i === 0) {
           $("#" + removex).children("." + removey).removeClass("blue");
-        // } else if (oldsegments.length > 1 && ateApple && i == oldsegments.length - 1) {
-        //   $("#" + removex).children("." + removey).removeClass("charmander");
         } else if (i == oldsegments.length - 1 && !ateApple) {
-          // $("#" + removex).children("." + removey).removeClass("charmander N S E W");
           if (this.game.level < 3) {
             $("#" + removex).children("." + removey).removeClass("charmander N S E W");
           } else if (this.game.level === 3 && ateApple) {
@@ -97,7 +94,6 @@
             $("#" + removex).children("." + removey).removeClass("charizard N S E W");
           }
         } else {
-          // $("#" + removex).children("." + removey).removeClass("charmander");
           if (this.game.level <= 3) {
             $("#" + removex).children("." + removey).removeClass("charmander");
           } else if (this.game.level >= 3 && this.game.level <= 9) {
@@ -115,7 +111,6 @@
       if (i === 0) {
         $("#" + snakeX).children("." + snakeY).addClass("blue " + this.game.board.snake.dir);
       } else {
-        // $("#" + snakeX).children("." + snakeY).addClass("charmander");
         if (this.game.level < 3) {
           $("#" + snakeX).children("." + snakeY).addClass("charmander");
         } else if (this.game.level >= 3 && this.game.level < 9) {
@@ -123,7 +118,6 @@
         } else {
           $("#" + snakeX).children("." + snakeY).addClass("charizard");
         }
-
       }
     }
     if ($(".apple").length === 0) {
